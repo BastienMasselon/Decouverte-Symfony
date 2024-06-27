dd1ace2c83a096c7e4b490bf5a9102de0f7419df
Bastien M <bastien.masselon@laposte.net>
2024-06-27 18:36:05 +0200

    Découverte du serializer et personnalisation d'un normalizer

--- src/Entity/Category.php
+++ src/Entity/Category.php
@@ -8,6 +8,7 @@ use Doctrine\Common\Collections\ArrayCollection;
 use Doctrine\Common\Collections\Collection;
 use Doctrine\ORM\Mapping as ORM;
 use Symfony\Bridge\Doctrine\Validator\Constraints\UniqueEntity;
+use Symfony\Component\Serializer\Annotation\Groups;
 use Symfony\Component\Validator\Constraints as Assert;
 
 #[ORM\Entity(repositoryClass: CategoryRepository::class)]
@@ -24,6 +25,7 @@ class Category
     #[Assert\Length(min: 5)]
     #[Assert\NotBlank()]
     #[BanWord()]
+    #[Groups(['recipes.show'])]
     private string $name = '';
 
     #[ORM\Column(length: 255)]
