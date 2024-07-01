f7e04e5eef1a6455d84b2a8269c12f6b745d169a
Bastien M <bastien.masselon@laposte.net>
2024-07-01 21:03:27 +0200

    Désérialisation, MapRequestPayload et MapQueryString

--- src/Controller/API/RecipeController.php
+++ src/Controller/API/RecipeController.php
@@ -2,19 +2,31 @@
 
 namespace App\Controller\API;
 
+use App\DTO\PaginationDTO;
 use App\Entity\Recipe;
 use App\Repository\RecipeRepository;
+use DateTimeImmutable;
+use Doctrine\ORM\EntityManager;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpKernel\Attribute\MapQueryString;
+use Symfony\Component\HttpKernel\Attribute\MapRequestPayload;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\Routing\Requirement\Requirement;
+use Symfony\Component\Serializer\Normalizer\AbstractNormalizer;
+use Symfony\Component\Serializer\SerializerInterface;
 
 class RecipeController  extends AbstractController
 {
-    #[Route("/api/recipes")]
-    public function index(RecipeRepository $repository, Request $request) 
+    #[Route("/api/recipes", methods: ["GET"])]
+    public function index(
+        RecipeRepository $repository, 
+        #[MapQueryString]
+        ?PaginationDTO $paginationDTO = null,
+        ) 
     {
-        $recipes = $repository->paginateRecipes($request->query->getInt('page', 1));
+        $recipes = $repository->paginateRecipes($paginationDTO?->page);
         return $this->json($recipes, 200, [], [
             'groups' => ['recipes.index']
         ]);
@@ -27,4 +39,24 @@ class RecipeController  extends AbstractController
             'groups' => ['recipes.index', 'recipes.show']
         ]);
     }
+
+    #[Route("/api/recipes", methods: ["POST"])]
+    public function create(
+        Request $request, 
+        #[MapRequestPayload(
+            serializationContext: [
+                'groups' => 'recipes.create'
+            ]
+        )]
+        Recipe $recipe, 
+        EntityManagerInterface $em
+    )
+    {
+        $recipe->setCreatedAt(new DateTimeImmutable());
+        $recipe->setUpdatedAt(new DateTimeImmutable());
+        $em->persist($recipe);
+        $em->flush();
+        return $this->json($recipe, 200);
+    }
+
 }
